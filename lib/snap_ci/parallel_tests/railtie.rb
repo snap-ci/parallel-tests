8b04c99ea13a23183e859d417e82a9468264a499
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-12-10 16:49:48 +0530

    Rename a file to make way for running unit tests

--- lib/snap_ci/parallel_tests/railtie.rb
+++ lib/snap_ci/parallel_tests/railtie.rb
@@ -2,7 +2,7 @@ module SnapCI
   module ParallelTests
     class Railtie < Rails::Railtie
       rake_tasks do
-        load 'snap_ci/parallel_tests/tasks/parallel_tests.rake'
+        load 'snap_ci/parallel_tests/tasks/parallel_specs.rake'
       end
     end
   end
